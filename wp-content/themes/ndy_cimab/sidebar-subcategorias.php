c07547f119b3c4f50642b6ae6aa6da499d03d1b2
Eric Bravo <eric.bravo.rod@gmail.com>
2015-07-24 12:03:39 -0500

    Quitando los contadores y signos + del widget de subcategorías Agregando link al título de galería de videos en el widget

--- wp-content/themes/ndy_cimab/sidebar-subcategorias.php
+++ wp-content/themes/ndy_cimab/sidebar-subcategorias.php
@@ -4,10 +4,12 @@
 		<?php $cat = get_the_category(); ?>
 		<?php $categories = get_categories( array( 'child_of' => 0, 'parent' => $cat[0]->cat_ID, 'orderby' => 'name', 'order' => 'ASC', 'taxonomy' => 'category', 'pad_counts' => true, 'hide_empty' => 0 ) ); ?>
 		<?php foreach ( $categories as $category ) : ?>
-		<li>
-			<p><a href="<?php echo get_category_link( $category->term_id ); ?>"><?php echo $category->name; ?></a></p>
-			<span class="fecha"><?php echo $category->count; ?></span>
-		</li>
+		<?php if ( $category->count > 0 ) : ?>
+			<li>
+				<p><a href="<?php echo get_category_link( $category->term_id ); ?>"><?php echo $category->name; ?></a></p>
+				<span class="fecha"></span>
+			</li>
+		<?php endif; ?>
 		<?php endforeach; ?>
 	</ul>
 </article>
--- wp-content/themes/ndy_cimab/sidebar-videoteca.php
+++ wp-content/themes/ndy_cimab/sidebar-videoteca.php
@@ -1,9 +1,11 @@
 <?php $videos = new WP_Query( array( 'post_type' => 'videos', 'posts_per_page' => 1, 'order' => 'DESC', 'orderby' => 'rand' ) ); ?>
 <?php if ( $videos->have_posts() ) : ?>
 	<article class="col-xs-12 col-sm-6 col-md-12">
-		<h2><?php echo __( 'Galería de Videos' ); ?></h2>
+		<a href="<?php echo bloginfo( 'url' ); ?>/videos" class="colours-black">
+			<h2><?php echo __( 'Galería de Videos' ); ?></h2>
+		</a>
 		<?php while ( $videos->have_posts() ) : $videos->the_post(); ?>
-			<a href="<?php echo bloginfo( 'url' ); ?>/videos">
+			<a href="<?php echo the_permalink(); ?>">
 				<?php if ( has_post_thumbnail() ) : ?>
 					<?php the_post_thumbnail( 'posts_home', array( 'class' => 'img-responsive img-full' ) ); ?>
 				<?php else : ?>
